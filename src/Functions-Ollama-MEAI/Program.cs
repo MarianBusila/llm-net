ae1cd4a68878925fddc6afdf9702a60916bb6ad2
mbusila-unity3d <marian.busila@unity3d.com>
2025-04-26 20:26:23 -0400

    Add functions calling sample using Ollama and MEAI

---
+++ src/Functions-Ollama-MEAI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.AI;
+
+IChatClient client = new OllamaChatClient(endpoint:"http://localhost:11434", modelId: "llama3.2:3b-instruct-fp16")
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build();
+    
+ChatOptions options = new ChatOptions
+{
+    Tools = [AIFunctionFactory.Create(GetTheWeather)]
+};
+
+var question = "Solve 2 + 2. Provide an accurate and short answer.";
+Console.WriteLine($"question: {question}");
+var response = await client.GetResponseAsync(question, options);
+Console.WriteLine($"response: {response}");
+
+Console.WriteLine();
+
+question = "Do I need an umbrella today?. Provide an accurate and short answer.";
+Console.WriteLine($"question: {question}");
+response = await client.GetResponseAsync(question, options);
+Console.WriteLine($"response: {response}");
+
+[Description("Get the weather")]
+static string GetTheWeather()
+{
+    Console.WriteLine("\tGet the weather function called");
+    var temperature = Random.Shared.Next(5, 20);
+    var conditions = Random.Shared.Next(0, 2) == 0 ? "sunny" : "rainy";
+    var weather = $"The weather is {temperature} degrees and {conditions}.";
+    Console.WriteLine($"\tGet the weather function returned: {weather}");
+    return weather;
+}
